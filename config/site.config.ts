37944367ecff4d309678fe5d8abd42ea396b8f26
psyonly <65434166+psyonly@users.noreply.github.com>
2023-05-23 21:12:26 +0800

    Update site.config.ts
    site infomation change.

--- config/site.config.ts
+++ config/site.config.ts
@@ -1,32 +1,32 @@
 const SITE_CONFIG = {
-	siteName: "Hyoban's Blog",
+	siteName: "psyonly's Blog",
 	siteLanguage: "zh-Hans",
-	description: "Hyoban 的个人博客，胡乱写些东西",
+	description: "psyonly 的个人博客，胡乱写些东西",
 	avatarPath: "/hyoban.png",
 	faviconPath: "/favicon.svg",
 	links: [
 		{
 			type: "GitHub",
-			url: "https://github.com/hyoban",
+			url: "https://github.com/psyonly",
 		},
 		{
 			type: "Twitter",
-			url: "https://twitter.com/0xhyoban",
+			url: "https://twitter.com/psyonly",
 		},
 		{
 			type: "Email",
-			url: "mailto:hi@hyoban.cc",
+			url: "mailto:hi@psyonly.cc",
 		},
 	],
 	codeTheme: {
 		light: "nord",
 		dark: "nord",
 	},
-	siteUrl: "https://hyoban.cc",
-	authorName: "Hyoban",
-	authorLink: "https://hyoban.cc",
-	authorEmail: "hi@hyoban.cc",
-	timeZone: "Asia/Shanghai",
+	siteUrl: "https://plog-dun.vercel.app",
+	authorName: "psyonly",
+	authorLink: "https://plog-dun.vercel.app",
+	authorEmail: "hi@psyonly.cc",
+	timeZone: "Asia/Jinan
 	source: "local",
 } as const
 
