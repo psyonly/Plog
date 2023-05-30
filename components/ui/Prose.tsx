cd0b4c0514db85793a68aa4e39ff65e71a7b3e81
psyonly <amazing_tree@qq.com>
2023-05-30 20:51:39 +0800

    fix: change prose width to 105.

--- components/ui/Prose.tsx
+++ components/ui/Prose.tsx
@@ -7,7 +7,7 @@ const Prose = (props: BasicProps) => {
 		<article
 			className={cn(
 				"flex flex-col gap-8 relative",
-				"mx-auto max-w-[90ch]",
+				"mx-auto max-w-[105ch]",
 				"prose prose-neutral dark:prose-invert",
 				"prose-h1:my-4 prose-h2:my-2 prose-h3:my-0 prose-h4:my-0 prose-h5:my-0 prose-h6:my-0",
 				"prose-ul:my-0 prose-ol:my-0 prose-p:my-0",
